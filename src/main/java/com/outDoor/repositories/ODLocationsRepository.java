ffe64e7e99eab212219179b4a01e85b3d6f925e3
xaxchkuget <xaxchkuget@gmail.com>
2021-10-21 05:58:37 -0700

    added updated tables and column names for table creation and data insertion

--- src/main/java/com/outDoor/repositories/ODLocationsRepository.java
+++ src/main/java/com/outDoor/repositories/ODLocationsRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.outDoor.models.ODLocations;
 
-public interface ODLocationsRepository extends JpaRepository<ODLocations, String> {
+public interface ODLocationsRepository extends JpaRepository<ODLocations, Integer> {
 	
 	
 	
