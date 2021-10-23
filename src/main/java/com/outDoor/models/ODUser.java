891d78d38775222789b0952b6ab6acbab5e428a3
xaxchkuget <xaxchkuget@gmail.com>
2021-10-23 13:12:16 -0700

    Adjusted User model to avoid dual table conflicts

--- src/main/java/com/outDoor/models/ODUser.java
+++ src/main/java/com/outDoor/models/ODUser.java
@@ -6,35 +6,39 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Entity
+@Table(name = "user")
 public class ODUser {
 	
 	@Id
-	@Column(table = "User", length = 12, nullable = false, unique = true, updatable= false)
-	private String userName;
+	@Column(name = "Username", length = 24, nullable = false)
+	private String username;
 	
-	@Column(table = "User", name = "email", length = 24, nullable = false)
+	@Column(name = "email", length = 24, nullable = false)
 	private String email;
 	
-	@Column(table = "User", name = "address", length = 50, nullable = false, updatable= false)
+	@Column(name = "address", length = 50, nullable = false, updatable= false)
 	private String address;
 	
-	@Column(table = "User", name = "DoB", nullable = false, updatable= false)
+	@Column(name = "DoB", nullable = false, updatable= false)
 	private Date DoB;
 	
-	@Column(table = "User", name = "primary_location", nullable = false)
+	@Column(name = "primary_location", nullable = false)
 	private Long primaryLocation;
 
+	@Column(name = "password", nullable = false)
+	private String password;
 //	@Column(table = "User", name = "friend_list_ID", nullable = false)
 //	private Long friendListId;
 	
